b3a90cf1e38e440aaa818ed80b571fc64a7e0782
kulloveth <kulloveth@gmail.com>
2021-10-14 21:02:18 +0100

    add the card and scroll property

--- app/src/main/java/eu/tutorials/dogprofilepage/ProfilePage.kt
+++ app/src/main/java/eu/tutorials/dogprofilepage/ProfilePage.kt
@@ -3,8 +3,12 @@ package eu.tutorials.dogprofilepage
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,56 +24,49 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfilePage() {
-    //Todo 2 : add a horizontal alignment with modifier to fill max size
-Column(horizontalAlignment = Alignment.CenterHorizontally,
-modifier = Modifier.fillMaxSize()) {
-    /**
-     *   Todo 1 Add a modifier to the Image element with a size,clip, border.
-     *   Also add a contentScale to crop the image
-     */
-  Image(painter =painterResource(id = R.drawable.husky),
-        contentDescription = "husky", modifier = Modifier
-          .size(100.dp)
-          .clip(CircleShape)
-          .border(width = 2.dp, color = Color.Red, shape = CircleShape),
-          contentScale = ContentScale.Crop)
-   Text(text = "Siberian Husky")
-    Text(text = "Germany")
-    /**
-     * Todo 4 add a Row Element with three Column containing 2 Text each,
-     * Todo 7: then we extract one column
-     * to create a reusable function ProfileStats, next we add horizontal arrangement  and modifier to fill maxWidth
-     * since we are focused on the horizontal axiz
-     */
+    //Todo 1: Add Card element with elevation and Modifier
+    Card(elevation = 6.dp,modifier = Modifier
+        .fillMaxSize()
+        .padding(top = 100.dp, bottom =100.dp, start = 16.dp, end = 16.dp)
+        .border(width = 2.dp, color = Color.White, shape =RoundedCornerShape(30.dp))){
+        //Todo 2: Add verticalScroll with vertical arrangement of Center
+        Column( Modifier.verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,horizontalAlignment = Alignment.CenterHorizontally){
+            Image(
+                painter =painterResource(id = R.drawable.husky), contentDescription = "husky",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .border(width = 2.dp, color = Color.Red, shape =CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            Text(text = "Siberian Husky", fontWeight = FontWeight.Bold)
+            Text(text = "Germany")
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ){
+                ProfileStats(count = "150", title = "Followers")
+                ProfileStats(count = "100", title = "Following")
+                ProfileStats(count = "30", title = "Posts")
+            }
 
-    Row(horizontalArrangement = Arrangement.SpaceEvenly,modifier = Modifier.fillMaxWidth()
-        .padding(16.dp)) {
-        ProfileStats(count = "150", title = "Followers")
-        ProfileStats(count = "100", title = "Following")
-        ProfileStats(count = "30", title = "Posts")
-    }
+            Row(horizontalArrangement = Arrangement.SpaceEvenly,modifier =
+            Modifier.fillMaxWidth().padding(top=16.dp)){
+                Button(onClick ={/*TODO*/}){
+                    Text(text = "Follow User")
+                }
 
-    //Todo 8: We add a Row for the buttons and space them evenly then add a modifer to fill max width
-    // and a padding as well
-    Row(horizontalArrangement = Arrangement.SpaceEvenly,modifier =
-    Modifier.fillMaxWidth().padding(top=16.dp)){
-        /** Todo 9:We Add a Button which automatically adds an onClick method because
-        all Button requires it but we leave it empty and a Text element to set the titile
-         */
-        Button(onClick ={}){
-            Text(text = "Follow User")
-        }
-        Button(onClick ={}){
-            Text(text = "Direct Message")
+                Button(onClick ={/*TODO*/}){
+                    Text(text = "Direct Message")
+                }
+            }
         }
     }
 }
-}
-/**
-Todo 5: Create a function with name ProfileStats and create the count and title parameters of type String.
-Todo 6: Copy one of the features and paste into the function block then change
-the values of the two text properties to use the two parameters count and title
- */
+
 @Composable
 fun ProfileStats(count: String, title: String) {
     Column(horizontalAlignment = Alignment.CenterHorizontally){
