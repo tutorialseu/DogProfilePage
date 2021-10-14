65076e397ff9d25fc7913a029d8227ea2c023b48
kulloveth <kulloveth@gmail.com>
2021-10-14 20:12:56 +0100

    create a Row with three column then Create a method to replace the columns

--- app/src/main/java/eu/tutorials/dogprofilepage/ProfilePage.kt
+++ app/src/main/java/eu/tutorials/dogprofilepage/ProfilePage.kt
@@ -3,6 +3,7 @@ package eu.tutorials.dogprofilepage
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -14,6 +15,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -34,7 +36,34 @@ modifier = Modifier.fillMaxSize()) {
           contentScale = ContentScale.Crop)
    Text(text = "Siberian Husky")
     Text(text = "Germany")
+//Todo 4 add a Row Element with three Column containing 2 Text each
+    Row {
+        Column(horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = "150", fontWeight = FontWeight.Bold)
+            Text(text = "Followers")
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = "100", fontWeight = FontWeight.Bold)
+            Text(text = "Following")
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = "30", fontWeight = FontWeight.Bold)
+            Text(text = "Posts")
+        }
+    }
 }
+}
+/**
+Todo 5: Create a function with name ProfileStats and create the count and title parameters of type String.
+Todo 6: Copy one of the features and paste into the function block then change
+the values of the two text properties to use the two parameters count and title
+ */
+@Composable
+fun ProfileStats(count: String, title: String) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = count, fontWeight = FontWeight.Bold)
+        Text(text = title)
+    }
 }
 
 @Preview(showBackground = true)
