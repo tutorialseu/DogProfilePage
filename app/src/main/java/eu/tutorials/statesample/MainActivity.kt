45ec8802135f1089eadbda2379e0b21900cff26b
kulloveth <kulloveth@gmail.com>
2021-10-19 17:17:01 +0100

    add a Text, TextField and Button to a Column

---
+++ app/src/main/java/eu/tutorials/statesample/MainActivity.kt
@@ -0,0 +1,53 @@
+package eu.tutorials.statesample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import eu.tutorials.statesample.ui.theme.StateSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StateSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+//Todo 1: Create a new compose Activity and change the content of Greeting method
+fun Greeting() {
+
+    //Todo 2: Add a column composable to arrange the composables vertically
+    Column{
+        Text(text = "" )
+        TextField(value ="", onValueChange = {
+        } )
+        Button(onClick = {
+        }) {
+            Text(text = "Display")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StateSampleTheme {
+        Greeting()
+    }
+}
